3178d02e63c004118bc856f7c7ee641ec0ea5441
tamago324 <tamago_pad@yahoo.co.jp>
2020-02-06 07:52:56 +0900

    ページネーション実装

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,48 @@
 <template>
   <div id="app">
-    <router-link :to="{ name: 'page', params: { page: 1 } }">1</router-link>
-    <router-link :to="{ name: 'page', params: { page: 2 } }">2</router-link>
-    <router-link :to="{ name: 'page', params: { page: 3 } }">3</router-link>
+    <b-pagination
+      :total="total"
+      :current="current"
+      per-page="10"
+      range-before="1"
+      range-after="1"
+      order="is-centered"
+      size="default"
+      aria-previous-label="Previous page"
+      aria-next-label="Next page"
+      aria-page-label="Page"
+      aria-current-label="Current page"
+    >
+      <b-pagination-button
+        slot-scope="props"
+        :page="props.page"
+        :id="`page${props.page.number}`"
+        tag="router-link"
+        :to="{name: 'page', params: {page: parseInt(props.page.number)}}"
+      >
+        {{ props.page.number }}
+      </b-pagination-button>
+
+      <b-pagination-button
+        slot="previous"
+        slot-scope="props"
+        :page="props.page"
+        tag="router-link"
+        :to="{name: 'page', params: {page: parseInt(props.page.number)}}"
+      >
+        <b-icon icon="angle-left" />
+      </b-pagination-button>
+
+      <b-pagination-button
+        slot="next"
+        slot-scope="props"
+        :page="props.page"
+        tag="router-link"
+        :to="{name: 'page', params: {page: parseInt(props.page.number)}}"
+      >
+        <b-icon icon="angle-right" />
+      </b-pagination-button>
+    </b-pagination>
     <router-view></router-view>
   </div>
 </template>
@@ -16,18 +56,32 @@ export default {
   data() {
     return {
       topStories: null,
+      total: 1, // 全部でいくつか
+      current: 1, // 現在のページ
     };
   },
 
   firebase: {
     topStories: api.child('topstories'),
   },
+
+  watch: {
+    topStories(newVal) {
+      this.total = newVal.length;
+      // // 取得できたら1ページに移動する
+      // this.$router.push({name: 'page', params: {page: 1}});
+    },
+
+    $route(to) {
+      this.current = to.params.page;
+    },
+  },
 };
 </script>
 
 <style>
 #app {
-  margin: 0 auto;
+  margin: 1rem auto;
   width: 820px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,15 +3,25 @@ import App from './App.vue';
 import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
 import {rtdbPlugin} from 'vuefire';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faAngleRight, faAngleLeft} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 
 import router from './router/index';
 
+library.add(faAngleRight, faAngleLeft);
+
+Vue.component('vue-fontawesome', FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
-Vue.use(Buefy);
+Vue.use(Buefy, {
+  defaultIconPack: 'fas',
+  defaultIconComponent: 'vue-fontawesome'
+});
 Vue.use(rtdbPlugin);
 
 new Vue({
   router,
   render: h => h(App),
-}).$mount('#app');
+}).$mount('#app')
